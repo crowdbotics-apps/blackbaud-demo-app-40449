292bf63e79998284b5305e0c74380bbaa22bc010
Crowdbotics <team@crowdbotics.com>
2023-05-04 15:13:27 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "blackbaud-demo-app-40449",
+  url: "https://blackbaud-demo-app-40449.botics.co",
+  api: "https://blackbaud-demo-app-40449.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
